3a82e7517da9d5b48db1b04826b8d8aee19f1600
Gustavo Miranda <oachgustavo@gmail.com>
2020-03-22 14:16:44 -0300

    v0.0.5

--- src/main/java/com/github/gustavovitor/maker_example/repository/specification/CarSpecification.java
+++ src/main/java/com/github/gustavovitor/maker_example/repository/specification/CarSpecification.java
@@ -3,9 +3,12 @@ package com.github.gustavovitor.maker_example.repository.specification;
 import com.github.gustavovitor.maker.repository.MongoSpecificationBase;
 import com.github.gustavovitor.maker_example.domain.Car;
 import com.github.gustavovitor.maker_example.domain.QCar;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.*;
 
 import javax.management.ReflectionException;
+
+import static java.util.Objects.nonNull;
 
 public class CarSpecification extends MongoSpecificationBase<Car> {
 
@@ -13,8 +16,14 @@ public class CarSpecification extends MongoSpecificationBase<Car> {
         super(car);
     }
 
+    public CarSpecification() { }
+
     @Override
     public Predicate toPredicate() {
-        return QCar.car.model.containsIgnoreCase(getObject().getModel());
+        BooleanBuilder builder = new BooleanBuilder();
+        if (nonNull(getObject().getModel())) {
+            builder.and(QCar.car.model.containsIgnoreCase(getObject().getModel()));
+        }
+        return builder;
     }
 }
